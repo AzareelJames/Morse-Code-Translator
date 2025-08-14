a3a0dab71023bc4393c574447b28d400e0a74843
AzareelJames <azareeljameslbasalo@gmail.com>
2025-08-14 19:26:04 +0800

    script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function mainloopAndCalculate(){
         code = ``;
 
         if(type.value === `Text -> Morse`){
-            for(let i of input.value.toLowerCase()){
+            for(let i of input.value.trim.toLowerCase()){
                 if(Char.indexOf(i) === -1){
                     code = `Invalid Input (Letters or Numbers only)`;
                     break;
@@ -51,7 +51,7 @@ function mainloopAndCalculate(){
                 code += `${Morse[Char.indexOf(i)]} `;
             }
         } else{
-            for(let i of input.value.split(` `)){
+            for(let i of input.value.trim.split(` `)){
                 if(Morse.indexOf(i) === -1){
                     code = `Invalid Morse Code`;
                     break;
@@ -72,5 +72,6 @@ function mainloopAndCalculate(){
         mainloopAndCalculate();
     }, 250);
 }
+
 
 mainloopAndCalculate();
